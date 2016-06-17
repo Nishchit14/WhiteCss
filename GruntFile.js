41648a4715349b8a41117e52d2db32c66ddf58ce
Nishchit Dhanani <dnishchit@gmail.com>
2016-06-17 11:43:21 +0530

    Versioning added

---
+++ GruntFile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'upstream',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        metadata: '',
+        regExp: false
+      }
+    },
+  })
+
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.registerTask('default', ['bump']);
+
+};
